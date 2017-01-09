76a45f931b3ce9a1cde63a05182a9df848bc6da4
IMGRBS <mixxdarkz2@gmail.com>
2017-01-09 20:27:57 +0700

    เพิ่มฟังก์ชั่น แสดงข้อมูลใน table หน้า leaderboard + ดึงข้อมูลจาก firebase

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,50 +1,41 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCRBB-Tk41WvH96-DRCBqZXH8ZRoaetwdc",
+  authDomain: "test-f013f.firebaseapp.com",
+  databaseURL: "https://test-f013f.firebaseio.com",
+  storageBucket: "test-f013f.appspot.com",
+  messagingSenderId: "907496327231"
+};
+firebase.initializeApp(config);
+
+const test = document.getElementById('test');
+const dbRef = firebase.database().ref();
+
+
 $(function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCRBB-Tk41WvH96-DRCBqZXH8ZRoaetwdc",
-    authDomain: "test-f013f.firebaseapp.com",
-    databaseURL: "https://test-f013f.firebaseio.com",
-    storageBucket: "test-f013f.appspot.com",
-    messagingSenderId: "907496327231"
-  };
-  firebase.initializeApp(config);
+  const input = $('input');
+  const button = $('a');
+  const tbody = $('tbody');
+  let i=0;
+
+  const usersRef = dbRef.child("users").orderByChild("score").on('value', snap => test.innerText = JSON.stringify(snap.val(), null, 3));
+  let getArrays = [];
+  getArrays = usersRef;
+  console.log(getArrays);
+    button.click(function() {
+      if (i<10) {
+        for(i=0;i<10;i++) {
+          const text = input.val();
+          input.val();
+          addToList(text);
+        }
+      }
+    })
 
 
-  const preUsers = document.getElementById('object');
-  
-  const userRef = firebase.database().ref().child('users');
+  function addToList(text) {
+    const tr ='<tr>' + '<td>'+ (i+1) + '</td>' + '<td class="hidden-sm-down">'+ "picurl" + '</td>' + '<td>'+ "Facebook Name"+ '</td>' + '<td>'+ "Score" + '</td>' + '</tr>';
+    tbody.append(tr);
+  }
 
-  userRef.on('value', snap => console.log(snap.val()));
-
-}());
-// $(function() {
-//   var input =$('input');
-//   var button =$('a');
-//   var ul=$('ul');
-//
-//   listenToFirebase();
-//
-//   button.click(function() {
-//     var text = input.val();
-//     input.val('');
-//     userRef.push(text);
-//   });
-//
-//   function listenToFirebase() {
-//     userRef.on('value', function(snapshot) {
-//       var users = snapshot.val();
-//       var i = 1;
-//       ul.empty();
-//       $.each(users, function(index, c) {
-//         addToList(parseInt(i)+': ' + c);
-//         i += 1;
-//       });
-//     });
-//   }
-//
-//   function addToList(text) {
-//     var li ='<li class ="list-group-item">' + text + '</li>';
-//     ul.append(li);
-//   }
-//
-// });
+});
