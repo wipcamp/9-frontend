113e3ca94cf95467809dc6ce5e9c418e4fd0aed6
Boat <boat46450@hotmail.com>
2017-01-17 21:48:36 +0700

    remove pan and add function slideActive

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
   });
 
   $(function(){
-    var hammer = new Hammer($("#con")[0]).on('swipeleft swiperight panleft panright panend pancancel', handleHammer);
+    var hammer = new Hammer($("#con")[0]).on('swipeleft swiperight', handleHammer);
   });
 
   $('.slide-control').append('<div class="prev-container" style="color: black">prev</div>');
@@ -115,21 +115,11 @@ function marginSlideControl(){
   $('.slide-control').css({"margin-left": + dif +"px"});
 }
 
-var $con = $('.slide')[page];
-var slideWidth = 0;
-var slideCount = $('.slide').length;
-var panBoundary = 0.25;
 function handleHammer(event) {
   switch (event.type) {
     case 'swipeleft':
     case 'swiperight':
       handleSwipe(event);
-      break;
-    case 'panleft':
-    case 'panright':
-    case 'panend':
-    case 'pancancel':
-      handlePan(event);
       break;
   }
 }
@@ -144,43 +134,28 @@ function handleSwipe(event) {
         break;
     }
   }
-}
-function outOfBound() {
-  var left = $('.slide:eq('+ Math.abs(page)+')').position().left;
-  return (page == 0 && left >= 0) ||
-         (page == slideCount - 1 && left <= -slideWidth * (slideCount - 1));
-}
-function handlePan(event) {
-  switch (event.type) {
-    case 'panleft':
-    case 'panright':
-      // Slow down at the first and last pane.
-      if (outOfBound()) {
-        event.deltaX *= 0.2;
-      }
-      setContainerOffsetX(-page * slideWidth + event.deltaX);
-      console.log(-page * slideWidth + event.deltaX);
-      break;
-    case 'panend':
-    case 'pancancel':
-      if (Math.abs(event.deltaX) > slideWidth * panBoundary) {
-        if (event.deltaX > 0) {
-          if($('.slide.active').length == 0) {
-            slide(-1);
-          }
-        } else {
-          if($('.slide.active').length == 0) {
-            slide(1);
-          }
-        }
-      } /*else {
-        self.showPane(currentPane);
-      }*/
-      break;
+  else if ($('.slide.active').length == 1) {
+    switch (event.direction) {
+      case Hammer.DIRECTION_LEFT:
+        slideActiveLeft()
+        break;
+      case Hammer.DIRECTION_RIGHT:
+        slideActiveRight()
+        break;
+    }
   }
 }
-function setContainerOffsetX(offsetX) {
-  $.each($('div.slide'), function (idx, val) {
-    $(val).css({"transform": "perspective(100px) translate3d(" + getTransform(val)[0] - offsetX + "px, -100px, -50px)"});
-  });
+function slideActiveLeft(){
+  $('.slide.active').removeClass('active');
+  $('.slide:eq('+Math.abs(page)+')').css({"transform": "perspective(100px) translate3d(0, -100px, -50px)"});
+  setTimeout(function() { slide(1); }, 1000);
+  setTimeout(function() { $('.slide:eq('+Math.abs(page)+')').addClass('active'); }, 1000);
+  setTimeout(function() { $('.slide:eq('+Math.abs(page)+')').css({"transform": "perspective(100px) translate3d(0, 0, 0)"}); }, 1000);
+}
+function slideActiveRight(){
+  $('.slide.active').removeClass('active');
+  $('.slide:eq('+Math.abs(page)+')').css({"transform": "perspective(100px) translate3d(0, -100px, -50px)"});
+  setTimeout(function() { slide(-1); }, 1000);
+  setTimeout(function() { $('.slide:eq('+Math.abs(page)+')').addClass('active'); }, 1000);
+  setTimeout(function() { $('.slide:eq('+Math.abs(page)+')').css({"transform": "perspective(100px) translate3d(0, 0, 0)"}); }, 1000);
 }
