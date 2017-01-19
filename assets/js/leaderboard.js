2c96b7f2b91fe2fa5decc8fd11e4cebd2dde966e
IMGRBS <mixxdarkz2@gmail.com>
2017-01-20 00:00:34 +0700

    - add page game center w/responsive - change bootrap table to datatable

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyCRBB-Tk41WvH96-DRCBqZXH8ZRoaetwdc",
-  authDomain: "test-f013f.firebaseapp.com",
-  databaseURL: "https://test-f013f.firebaseio.com",
-  storageBucket: "test-f013f.appspot.com",
-  messagingSenderId: "907496327231"
+    apiKey: "AIzaSyCRBB-Tk41WvH96-DRCBqZXH8ZRoaetwdc",
+    authDomain: "test-f013f.firebaseapp.com",
+    databaseURL: "https://test-f013f.firebaseio.com",
+    storageBucket: "test-f013f.appspot.com",
+    messagingSenderId: "907496327231"
 };
 firebase.initializeApp(config);
 
@@ -16,47 +16,44 @@ const dbRef = firebase.database().ref();
 
 // main function
 $(function() {
-  const input = $('input');
-  const button = $('a');
-  const tbody = $('tbody');
-  let i=0;
-  let j=9;
+    const input = $('input');
+    const button = $('a');
+    const tbody = $('tbody');
+    let i = 0;
+    let j = 9;
 
-// timeout
-var delay=2250;
+    // timeout
+    var delay = 2250;
 
-// get data form database ( child - users )
-// 1000 = 1 second
-  const usersRef = dbRef.child("users").orderByChild("score").limitToLast(10);
-// const getUsers = usersRef.on('value', snap => test.innerText = JSON.stringify(snap.val(), null, 3));
+    // get data form database ( child - users )
+    // 1000 = 1 second
+    const usersRef = dbRef.child("users").orderByChild("score").limitToLast(100);
+    // const getUsers = usersRef.on('value', snap => test.innerText = JSON.stringify(snap.val(), null, 3));
 
-// get score < only > !
-  let scores = [];
-  let names = [];
-  let urlpics = [];
-  usersRef.on("child_added", function(data) {
-    let users = data.val();
-    names.push(users.name);
-    scores.push(users.score);
-    urlpics.push(users.urlpic);
-  });
+    // get score < only > !
+    let users = [];
 
-  $(document).ready(function() {
-    setTimeout(function() {
-      if (i<10) {
-        for(i=0;i<10;i++) {
-          input.val();
-          addToList();
-        }
-      }
-    }, delay);
-  });
+    usersRef.on("child_added", function(data) {
+        users.push(data.val());
+    });
 
-  // add to table
-  function addToList(text) {
-    const tr ='<tr>' + '<th scope="row" class="game-forCenter" >'+ (i+1) + '</th>' + '<td class="hidden-sm-down game-forCenter">'+ '<img src="' + (urlpics[j]) + '" alt="' + (names[j]) + '" class="rounded-circle" />' + '</td>' + '<td class="game-forCenter">'+ names[j] + '</td>' + '<td>'+ scores[j] + '</td>' + '</tr>';
-    tbody.append(tr);
-    j--;
-  }
-
+// console.log(users);
+    $(document).ready(function() {
+        setTimeout(function() {
+            $('.game-tbl').DataTable({
+                data: users,
+                order: [[ 2 , 'desc' ]],
+                columns: [
+                    // { data: 'uid'},
+                    { data: 'urlpic',
+                    render: function ( data , type  , row){
+                      return '<img src="'+ data + '" class="rounded-circle">';
+                      }
+                    },
+                    {  data: 'name'   },
+                    {  data: 'score' }
+                ]
+            });
+        }, delay);
+    });
 });
