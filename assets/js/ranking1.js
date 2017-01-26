a66d173699e991f95ac6a023106a523debf46531
imgrbs <mixxdarkz2@gmail.com>
2017-01-27 00:47:02 +0700

    merge front_game -> mainFunc-OOP

---
+++ assets/js/ranking1.js
@@ -0,0 +1,97 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCRBB-Tk41WvH96-DRCBqZXH8ZRoaetwdc",
+    authDomain: "test-f013f.firebaseapp.com",
+    databaseURL: "https://test-f013f.firebaseio.com",
+    storageBucket: "test-f013f.appspot.com",
+    messagingSenderId: "907496327231"
+};
+firebase.initializeApp(config);
+
+jQuery.fn.dataTableExt.oApi.fnDataUpdate = function ( oSettings, nRowObject, iRowIndex )
+{
+    jQuery(nRowObject).find("TD").each( function(i) {
+          var iColIndex = oSettings.oApi._fnVisibleToColumnIndex( oSettings, i );
+          oSettings.oApi._fnSetCellData( oSettings, iRowIndex, iColIndex, jQuery(this).html() );
+    } );
+};
+
+// id test in testfirebase
+// const test = document.getElementById('test');
+
+// get reference
+const dbRef = firebase.database().ref();
+
+// main function
+$(function() {
+    const input = $('input');
+    const button = $('a');
+    const tbody = $('tbody');
+    let i = 0;
+    let j = 9;
+
+    // timeout
+    let delay = 5000;
+
+    // get data form database ( child - users )
+    // 1000 = 1 second
+    const usersRef = dbRef.child("users").orderByChild("score").limitToLast(100);
+    // const getUsers = usersRef.on('value', snap => test.innerText = JSON.stringify(snap.val(), null, 3));
+
+    // get score < only > !
+    let users = [];
+    let temp= [];
+    usersRef.on("child_added", function(data) {
+      // users.push(data.val());
+      users.push(data.val());
+    //   usersRef.on("value", function(data) {
+    //
+    // });
+    });
+
+    $(document).ready(function() {
+      // New delay
+      if(users !== temp ) {
+        delay=2500;
+        setTimeout(function() {
+          var t = $('.game-tbl').DataTable({
+            data: users,
+            responsive: true,
+            columnDefs: [
+              {
+                  orderable : false,
+                  className: "hidden-xs-down",
+                  targets :[1]
+              },
+              {
+                  orderable : false,
+                  targets :[0]
+              },
+            ],
+            order: [[ 3 , 'desc' ]],
+            columns: [
+              { data: null,
+              render: function ( data , type , row){
+                return '<div '+ data +'>';
+              }
+            },
+              { data: 'urlpic',
+              render: function ( data , type  , row){
+                return '<img src="'+ data + '" class="rounded-circle">';
+              }
+            },
+            {  data: 'name' },
+            {  data: 'score' }
+          ]
+        });
+
+        t.on( 'order.dt search.dt', function () {
+          t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+          } );
+        } ).draw();
+
+      }, delay);
+      }
+  });
+});
