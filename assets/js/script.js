e07b57bc9e6751ceec2424b44a2e88fba2e9574a
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-02-08 22:16:21 +0700

    edit script delay , edit size island in tablet

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     $('.ship-main').css({"left": "-1em"});
   },3000);
 
-  $('.card-btn-back').click(function (event) {
+  $('.card-btn-back-home').click(function (event) {
     $('.ship-main').css({left: '75em'});
     setTimeout(function(){
       $('.con').addClass('idle');
@@ -52,6 +52,22 @@ $(document).ready(function(){
       $('.ship-main').css({left: '-30em'});
       event.stopPropagation();
     },2300);
+  });
+
+  $('.card-btn-back').click(function (event) {
+    $('.con').addClass('idle');
+    $('.slide').addClass('animate');
+
+    // $('.wave1, .wave2, .wave3').addClass('pause');
+    $('.cloud1, .cloud2, .cloud3').addClass('pause');
+    // $('.ship').addClass('pause');
+    // $('.ship-main').addClass('pause');
+    $('.slide.active').css({"transform": "perspective(100px) translate3d(0, -100px, -50px)"});
+    $('.slide.active').removeClass('active');
+    $('.slide:eq('+carousel.getCurrentSlide()+')').addClass('hover');
+    $('.ship-main').removeClass('transition1');
+    $('.ship-main').css({left: '-30em'});
+    event.stopPropagation();
   });
 
   $('div.slide').click(function(event) {
