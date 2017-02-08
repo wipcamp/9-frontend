de2226416f4d8568615166b9559657c012a09492
Boat <boat46450@hotmail.com>
2017-02-08 17:42:39 +0700

    cloud return

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,11 +32,11 @@ $(document).ready(function(){
   $('.card-btn-back').click(function (event) {
     $('.con').addClass('idle');
     $('.slide').addClass('animate');
-    /*$('.slide:eq('+carousel.getCurrentSlide()+')').addClass('hover');
-    $('.wave1, .wave2, .wave3').addClass('pause');
+
+    // $('.wave1, .wave2, .wave3').addClass('pause');
     $('.cloud1, .cloud2, .cloud3').addClass('pause');
-    $('.ship').addClass('pause');
-    $('.ship-main').addClass('pause');*/
+    // $('.ship').addClass('pause');
+    // $('.ship-main').addClass('pause');
     $('.slide.active').css({"transform": "perspective(100px) translate3d(0, -100px, -50px)"});
     $('.slide.active').removeClass('active');
     $('.slide:eq('+carousel.getCurrentSlide()+')').addClass('hover');
@@ -64,10 +64,10 @@ $(document).ready(function(){
 
     beforeAnimate().done(afterAnimate());
     $('.slide').removeClass('animate');
-    /*$('.wave1, .wave2, .wave3').removeClass('pause');
+    // $('.wave1, .wave2, .wave3').removeClass('pause');
     $('.cloud1, .cloud2, .cloud3').removeClass('pause');
-    $('.ship').removeClass('pause');
-    $('.ship-main').removeClass('pause');*/
+    // $('.ship').removeClass('pause');
+    // $('.ship-main').removeClass('pause');
   });
   $('.slide:eq(' + carousel.getCurrentSlide() + ')').removeClass('hover');
   countDown();
